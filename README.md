1f1fc2b187ee38010dc497556890706ebf8d094e
za5en <79802829+za5en@users.noreply.github.com>
2025-06-06 23:43:14 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# financial_manager
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/financial_manager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.financial_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
