a0f35f9d8381869db5f721e3f1a2f62411bfca13
za5en <79802829+za5en@users.noreply.github.com>
2025-06-14 02:50:00 +0400

    pages draft

---
+++ lib/view/pages/account_view.dart
@@ -0,0 +1,25 @@
+import 'package:financial_manager/view/widgets/f_appbar.dart';
+import 'package:financial_manager/view/widgets/f_navbar.dart';
+import 'package:financial_manager/view/widgets/f_svg.dart';
+import 'package:flutter/material.dart';
+
+class AccountView extends StatelessWidget {
+  const AccountView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: FAppbar(
+        title: 'Мой счет',
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: InkWell(child: FSvg(assetName: 'assets/images/edit.svg')),
+          ),
+        ],
+      ),
+      body: Center(child: Text('Financial Manager')),
+      bottomNavigationBar: FNavbar(),
+    );
+  }
+}
--- lib/view/pages/settings_view.dart
+++ lib/view/pages/settings_view.dart
@@ -1,12 +1,14 @@
+import 'package:financial_manager/view/widgets/f_appbar.dart';
 import 'package:financial_manager/view/widgets/f_navbar.dart';
 import 'package:flutter/material.dart';
 
-class Expenses extends StatelessWidget {
-  const Expenses({super.key});
+class SettingsView extends StatelessWidget {
+  const SettingsView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: FAppbar(title: 'Настройки'),
       body: Center(child: Text('Financial Manager')),
       bottomNavigationBar: FNavbar(),
     );
