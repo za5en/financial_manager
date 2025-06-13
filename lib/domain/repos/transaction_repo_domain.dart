a53badd1c188909987b443fc2ebcfcedc01845d0
za5en <79802829+za5en@users.noreply.github.com>
2025-06-13 22:12:15 +0400

    abstract repositories

---
+++ lib/domain/repos/transaction_repo_domain.dart
@@ -0,0 +1,20 @@
+import 'package:financial_manager/data/models/transaction/transaction_model.dart';
+import 'package:financial_manager/data/models/transaction/transaction_request_model.dart';
+import 'package:financial_manager/data/models/transaction/transaction_response_model.dart';
+
+abstract class TransactionRepoDomain {
+  Future<TransactionModel> createTransaction(
+    TransactionRequestModel transactionRequest,
+  );
+  Future<TransactionResponseModel> getTransactionById(int id);
+  Future<TransactionResponseModel> updateTransaction(
+    int id,
+    TransactionRequestModel transactionRequestModel,
+  );
+  Future<void> deleteTransaction(int id);
+  Future<List<TransactionResponseModel>> getTransactionsByPeriod(
+    int accountId,
+    DateTime? startDate,
+    DateTime? endDate,
+  );
+}
