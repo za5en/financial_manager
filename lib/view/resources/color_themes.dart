d69163a3b6e7e7da0b500d9b90026541cd115120
za5en <79802829+za5en@users.noreply.github.com>
2025-06-21 03:00:20 +0400

    date picker theme

--- lib/view/resources/color_themes.dart
+++ lib/view/resources/color_themes.dart
@@ -23,6 +23,34 @@ final _light = ThemeData(
       return const TextStyle(fontWeight: FontWeight.w500, fontSize: 12);
     }),
   ),
+  datePickerTheme: DatePickerThemeData(
+    backgroundColor: Color.fromRGBO(212, 250, 230, 1),
+    dayBackgroundColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Color.fromRGBO(42, 232, 129, 1);
+      }
+      return Colors.transparent;
+    }),
+    todayBackgroundColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Color.fromRGBO(42, 232, 129, 1);
+      }
+      return Colors.transparent;
+    }),
+    todayForegroundColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white;
+      }
+      return Color.fromRGBO(42, 232, 129, 1);
+    }),
+    todayBorder: BorderSide(color: Color.fromRGBO(42, 232, 129, 1)),
+    cancelButtonStyle: ButtonStyle().copyWith(
+      foregroundColor: WidgetStateProperty.all<Color>(Colors.black),
+    ),
+    confirmButtonStyle: ButtonStyle().copyWith(
+      foregroundColor: WidgetStateProperty.all<Color>(Colors.black),
+    ),
+  ),
 );
 
 final _dark = ThemeData(fontFamily: 'Roboto');
