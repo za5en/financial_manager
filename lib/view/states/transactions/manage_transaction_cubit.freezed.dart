7562f4141b65349314de6083b95254a2df534292
za5en <79802829+za5en@users.noreply.github.com>
2025-07-19 02:39:01 +0400

    gen files

--- lib/view/states/transactions/manage_transaction_cubit.freezed.dart
+++ lib/view/states/transactions/manage_transaction_cubit.freezed.dart
@@ -1,6 +1,5 @@
-// dart format width=80
-// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
@@ -47,6 +46,148 @@ class $ManageTransactionStateCopyWith<$Res>  {
 $ManageTransactionStateCopyWith(ManageTransactionState _, $Res Function(ManageTransactionState) __);
 }
 
+
+/// Adds pattern-matching-related methods to [ManageTransactionState].
+extension ManageTransactionStatePatterns on ManageTransactionState {
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>({TResult Function( EditLoading value)?  loading,TResult Function( EditContent value)?  content,TResult Function( EditError value)?  error,required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case EditLoading() when loading != null:
+return loading(_that);case EditContent() when content != null:
+return content(_that);case EditError() when error != null:
+return error(_that);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult map<TResult extends Object?>({required TResult Function( EditLoading value)  loading,required TResult Function( EditContent value)  content,required TResult Function( EditError value)  error,}){
+final _that = this;
+switch (_that) {
+case EditLoading():
+return loading(_that);case EditContent():
+return content(_that);case EditError():
+return error(_that);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>({TResult? Function( EditLoading value)?  loading,TResult? Function( EditContent value)?  content,TResult? Function( EditError value)?  error,}){
+final _that = this;
+switch (_that) {
+case EditLoading() when loading != null:
+return loading(_that);case EditContent() when content != null:
+return content(_that);case EditError() when error != null:
+return error(_that);case _:
+  return null;
+
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>({TResult Function()?  loading,TResult Function( ManageTransactionViewModel content)?  content,TResult Function( Exception error)?  error,required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case EditLoading() when loading != null:
+return loading();case EditContent() when content != null:
+return content(_that.content);case EditError() when error != null:
+return error(_that.error);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult when<TResult extends Object?>({required TResult Function()  loading,required TResult Function( ManageTransactionViewModel content)  content,required TResult Function( Exception error)  error,}) {final _that = this;
+switch (_that) {
+case EditLoading():
+return loading();case EditContent():
+return content(_that.content);case EditError():
+return error(_that.error);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>({TResult? Function()?  loading,TResult? Function( ManageTransactionViewModel content)?  content,TResult? Function( Exception error)?  error,}) {final _that = this;
+switch (_that) {
+case EditLoading() when loading != null:
+return loading();case EditContent() when content != null:
+return content(_that.content);case EditError() when error != null:
+return error(_that.error);case _:
+  return null;
+
+}
+}
+
+}
 
 /// @nodoc
 
